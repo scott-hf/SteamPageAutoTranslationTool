5e3e4b3f986646478730c0b44905aab768b7827f
scott_dunbar <scottlindsayjr@gmail.com>
2024-05-02 12:02:40 -0500

    adding missing source files for translation logic

--- translateEverything.bat
+++ translateEverything.bat
@@ -3,3 +3,5 @@ echo Have you placed storepage_base.json and appname_base.json into the source f
 set /p ready="Press any key when ready"
 python translateStorePages.py
 python translateStoreNames.py
+
+set /p ready="Done"
